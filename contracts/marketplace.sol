f9e06b0fd9e927f77134d32bdda3ba4491d2a576
minjooki <minjooki@andrew.cmu.edu>
2024-12-21 16:09:15 -0500

    add marketplace and hardhat

---
+++ contracts/marketplace.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Marketplace is Ownable {
+    address public multisigContract;
+
+    struct Listing {
+        address seller;
+        address nftContract;
+        uint tokenId;
+        uint price;
+    }
+
+    mapping(uint256 => Listing) public listings;
+
+    event ItemListed(
+        address indexed seller,
+        address indexed nftContract,
+        uint indexed tokenId,
+        uint price
+    );
+
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    constructor(address _multisigContract) {
+        multisigContract = _multisigContract;
+    }
+
+    modifier onlyMultisig() {
+        require(msg.sender == multisigContract, "Only multisig is authorized");
+        _;
+    }
+
+    function listItem(address nftContract, uint tokenId, uint price) external onlyMultisig {
+        IERC721 nft = IERC721(nftContract);
+        require(nft.ownerOf(tokenId) == msg.sender);
+
+        listings[tokenId] = Listing({
+            seller: msg.sender,
+            price: price,
+            nftContract: nftContract,
+            tokenId: tokenId
+        });
+
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+
+    }
+
+    function buyItem(address nftContract, uint tokenId) external isPayable {
+        Listing memory listing = listings[tokenId];
+        require(msg.value >= listing.price, "Not enough funds to purchase");
+
+        IERC721(nftContract).safeTransferFrom(listing.seller, msg.sender, tokenId);
+        delete listings[tokenId];
+        payable(listing.seller).transfer(listing.price);
+        
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+
+}
