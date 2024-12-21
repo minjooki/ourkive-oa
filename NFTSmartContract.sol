a1672ed1bca2e32f5b7c79760394432a95240b71
minjooki <minjooki@andrew.cmu.edu>
2024-12-21 15:31:35 -0500

    add multisig as admin

--- NFTSmartContract.sol
+++ NFTSmartContract.sol
@@ -2,19 +2,34 @@
 pragma solidity ^0.8.0;
 
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract NewItem is ERC721 {
+contract NewItem is ERC721, Ownable {
     uint private _tokenIdCounter;
+    address public multisigContract;
 
-    constructor() ERC721("NewItem", "MNFT") {
+    event MintRequested(address to, uint tokenId);
+
+    modifier onlyMultisig() {
+        require(msg.sender == multisigContract, "Only multisig is authorized to mint");
+        _;
     }
 
-    function mint(address to, string tokenURI) public {
+    constructor(address _multisigContract) ERC721("NewItem", "MNFT") {
+        tokenIdCounter = 1;
+        multisigContract = _multisigContract;
+    }
+
+    function mint(address to, string tokenURI) public onlyMultisig {
         uint tokenId = _tokenIdCounter;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
         _tokenIdCounter += 1;
+        emit MintRequested(to, tokenId);
+    }
+
+    function setMultisigContract(address _multisigContract) external onlyOwner {
+        multisigContract = _multisigContract;
     }
 }
